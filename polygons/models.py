6d8a8ac1d661a566db1c8db45e42f6b2fa2a3346
muayyadayesh <muayyadayesh@gmail.com>
2022-11-25 21:13:37 +0200

    models initialization

--- polygons/models.py
+++ polygons/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+# Provider models
+
+
+class Provider(models.Model):
+    # Language choices
+    LANGUAGES = [
+        ('EN', 'EN'),
+        ('FR', 'FR'),
+        ('ES', 'ES')
+    ]
+    # Currency choices
+    CURRENCIES = [
+        ('USD', 'USD'),
+        ('EUR', 'EUR'),
+    ]
+
+    name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=254)
+    phone = models.PhoneNumberField()
+    language = models.CharField(choices=LANGUAGES, default='EN')
+    currency = models.CharField(choices=CURRENCIES, default='EN')
+
+    def __str__(self):
+        return self.name
+
+
+class ServiceArea(models.Model):
+    name = models.CharField(max_length=50)
+    price = models.DecimalField(max_digits=50, decimal_places=2)
+    geo_info = models.JSONField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
